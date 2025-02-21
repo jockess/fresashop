a12068bfe9d221ef2616c6ff6f8f326734600579
Jesse Reyes <jockess@gmail.com>
2025-02-21 11:59:35 -0600

    Actualizaciones de pantallas

--- chunks/js/ciclo-compras.js
+++ chunks/js/ciclo-compras.js
@@ -1,19 +1,19 @@
 //Build Tabulator
 var table = new Tabulator("#lista-compras-table", {
     ajaxURL:"chunks/js/ciclo-compras.json",
-    pagination:"local",
-    paginationSize:10,
-    paginationSizeSelector:[10, 15, 20, 25],
     layout:"fitColumns",
     height:"100%",
     maxHeight:"100%",
     columns:[
-    {title:"Ciclo", field:"ciclo", width:90, hozAlign:"center"},
-    {title:"Mes", field:"mes", hozAlign:"left", widthGrow:1},
-    {title:"Fecha", field:"fecha", sorter:"date", widthGrow:1},
-    {title:"Articulos", field:"articulos", hozAlign:"left", width:130},
-    {title:"Total", field:"total", widthGrow:1},
-    {title:"Ubicacion", field:"ubicacion", hozAlign:"left", widthGrow:1},
-    {title:"Canal", field:"canal", hozAlign:"left", widthGrow:1},
+    {title:"Fecha", field:"fecha", widthGrow:1, hozAlign:"center"},
+    {title:"Tienda", field:"tienda", hozAlign:"left", widthGrow:1},
+    {title:"Status", field:"status", widthGrow:1},
+    {title:"Cliente", field:"cliente", hozAlign:"left", width:130},
+    {title:"Qty", field:"cantidad", width:90,},
+    {title:"Producto", field:"producto", hozAlign:"left", widthGrow:3},
+    {title:"Precio", field:"precio", hozAlign:"left", widthGrow:1},
     ],
+});
+table.on("rowClick", function(e, row){
+    $('#editModal').modal('show');
 });
